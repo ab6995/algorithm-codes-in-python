54aa9a064e87920a6017260e05d005651d243a9b
ab6995 <ab6995@nyu.edu>
2017-05-23 23:54:24 -0400

    sum of two integers

---
+++ sum python.py
@@ -0,0 +1,11 @@
+"""There are  lines of input, and each line contains a single integer.
+
+Output Format
+
+Code prints the sum calculated and returned by solveMeFirst ."""
+
+def solveMeFirst(a,b):
+    x = a + b 
+    return x
+print (solveMeFirst(3,2))   
+    
