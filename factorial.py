6f5ee2488e5eb0765e86653bcc807b041af19fea
ab6995 <ab6995@nyu.edu>
2017-06-13 21:57:03 -0400

    factorial

---
+++ factorial.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  5 15:08:12 2017
+
+@author: ashishbansal
+problem:
+
+You are given an integer N. Print the factorial of this number.
+
+
+
+"""
+
+import sys
+from math import factorial
+
+n = int(input().strip())
+print(factorial(n))
